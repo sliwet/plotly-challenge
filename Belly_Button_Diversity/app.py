3ee8de7c6f3da4368839ec2331e5778f2251e255
Daewon Kwon <sliwet@gmail.com>
2019-12-04 00:26:53 -0500

    Fixed heroku upload settings

--- Belly_Button_Diversity/app.py
+++ Belly_Button_Diversity/app.py
@@ -16,7 +16,12 @@ app = Flask(__name__)
 #################################################
 # Database Setup
 #################################################
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db/bellybutton.sqlite"
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI', '') or "sqlite:///db/bellybutton.sqlite"
+# Goto Heroku settings - Reveal Conf Vars 
+# Key = SQLALCHEMY_DATABASE_URI
+# Value = sqlite:///db/bellybutton.sqlite
+
 db = SQLAlchemy(app)
 
 engine = db.engine
